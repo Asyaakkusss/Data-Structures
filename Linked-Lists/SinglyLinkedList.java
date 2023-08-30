0c72031c2c7814dcaa0df515cccca24c28d164bd
Asya <aya29@case.edu>
2023-08-29 22:05:10 -0400

    insert node at back method completed

--- Linked-Lists/SinglyLinkedList.java
+++ Linked-Lists/SinglyLinkedList.java
@@ -1,6 +1,24 @@
 
 public class SinglyLinkedList<T> {
 
-    Node<T> firstNode; 
+    Node<T> first_node; 
+
+    public SinglyLinkedList() {
+        this.first_node = null; 
+    }
+
+    public void insert_node_at_back (T inserted_value) {
+        Node<T> new_node = new Node<T>(inserted_value); 
+        
+        if (first_node == null)
+            first_node = new_node; 
+        else {
+            Node<T> ptr = null; 
+            while (ptr.next_node != null) {
+                ptr = ptr.next_node; 
+            }
+            ptr.next_node = new_node; 
+        }
+    }
 
 }
